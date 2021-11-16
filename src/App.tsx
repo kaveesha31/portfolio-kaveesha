374feeafecfa0e36f2034c4ab64762c27dad7178
kaveesha31 <kaveesha.rathnaka@gmail.com>
2021-11-16 01:47:28 -0800

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          kaveesha
+          Kaveesha Rathnayaka
         </a>
       </header>
     </div>
